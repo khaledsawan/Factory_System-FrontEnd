70d75d0e49373754b2122a651cae7526dac73998
Hornet7 <khaled963sawan@gmail.com>
2022-06-10 07:31:42 -0700

    set project structure

--- lib/Screen/testpage.dart
+++ lib/Screen/testpage.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+
 class TestPage extends StatelessWidget {
   const TestPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.brown,
-    child:Text('Test'));
+    return Container(color: Colors.brown, child: Text('Test'));
   }
 }
